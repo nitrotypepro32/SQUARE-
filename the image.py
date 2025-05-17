f1bdd10aea851bbd079c04de67549acc3ef75ef9
nitrotypepro32 <pskreddy@gmail.com>
2025-05-16 19:41:44 -0500

    Add files via upload

---
+++ the image.py
@@ -0,0 +1,9 @@
+print ("The full sqaure patern of hashtags")
+n = int(input("Enter the number of rows: "))    
+for i in range(1, n + 1):
+    j = 1
+    while j <= n:
+        print("#", end=" ")
+        j += 1
+    print()
+
